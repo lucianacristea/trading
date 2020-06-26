25bd4d9fddef7b74ecc524b69145722424b9daa0
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-26 20:05:56 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-  df=pd.read_csv('Curs_valutar.csv')
-  df['CHF´].plot()
+  df=pd.read_csv("Curs_valutar.csv")
+  df["CHF"].plot()
   plt.show()
   
 if __name__=="__main__":
