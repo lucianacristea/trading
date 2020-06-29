b4c6406b5bd72f9993f1d2a370e646750c574119
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-29 17:09:06 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 def test_run():
   df=pd.read_csv("Curs_valutar1.csv", index_col="Data", parse_dates=True)
   # convert the 'Date' column to datetime format 
-  df['Data']= pd.to_datetime(df['Data']) 
   print(df.head())
+  print (df.dtypes)
+  df['Data']= pd.to_datetime(df['Data']) 
   ###df['CHF'].plot()
   df[['CHF','EUR']].plot()
   plt.show()
