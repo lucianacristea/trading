c5b8b743d37b2d31737092e5ea50ad2c36c92e69
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-29 16:43:54 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 def test_run():
   df=pd.read_csv("Curs_valutar1.csv")
   print(df.head())
-  df['CHF'].plot()
+  ###df['CHF'].plot()
+  df[['CHF','EUR']].plot()
   plt.show()
   
 if __name__=="__main__":
