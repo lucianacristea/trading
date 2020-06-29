5823f3f762f932235cd262c8e1d4cdb1973c760c
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-29 16:54:56 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-  df=pd.read_csv("Curs_valutar1.csv")
+  df=pd.read_csv("Curs_valutar1.csv", index_col="Data", parse_dates=True)
   print(df.head())
   ###df['CHF'].plot()
   df[['CHF','EUR']].plot()
