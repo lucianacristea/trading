a6829ed896929a9ccc81a05e58ce07241851b5a7
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-30 10:12:48 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -17,6 +17,43 @@ def plot_data(df, title, fontsize, xlabel, ylabel):
 def normalize_data(df):
     return df/df.iloc[0,:]
 
+def get_rolling_mean(values, window):
+    """Return rolling mean of given values, using specified window size."""
+    return pd.rolling_mean(values, window=window)
+
+
+def get_rolling_std(values, window):
+    """Return rolling standard deviation of given values, using specified window size."""
+    # TODO: Compute and return rolling standard deviation
+    return pd.rolling_std(values, window)
+
+def get_bollinger_bands(rm, rstd):
+    """Return upper and lower Bollinger Bands."""
+    # TODO: Compute upper_band and lower_band
+    upper_band=rm+2*rstd
+    lower_band=rm-2*rstd
+    return upper_band, lower_band
+
+def plot_bollinger_data(df, label, window, title, fontsize, xlabel, ylabel):
+    # Compute Bollinger Bands
+    # 1. Compute rolling mean
+    rm_SPY = get_rolling_mean(df[label], window=window)
+
+    # 2. Compute rolling standard deviation
+    rstd_SPY = get_rolling_std(df[label], window=window)
+
+    # 3. Compute upper and lower bands
+    upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
+    
+    # Plot raw SPY values, rolling mean and Bollinger Bands
+    ax = df[label].plot(title=title, label=label)
+    rm_SPY.plot(label='Rolling mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)  
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
+
 def test_run():
   df=pd.read_csv("Curs_valutar.csv", index_col="Dates", parse_dates=True)
   # convert the 'Date' column to datetime format 
@@ -29,10 +66,9 @@ def test_run():
   ###df=df/df.loc[0]
   ###df.plot()
   # Slice and plot
-  ###plot_selected(df, ['CHF','EUR','GBP','USD'], '03.01.2020', '29.06.2020')
-  ###plot_data(normalize_data(df), "Curs valutar", 2, "Dates", "Price")
-  plot_data(normalize_data( plot_selected(df, ['CHF','EUR','GBP','USD'], '03.01.2020', '29.06.2020')), "Curs valutar", 2, "Dates", "Price")
-  plot_data(plot_selected(df, ['CHF','EUR','GBP','USD'], '03.01.2020', '29.06.2020'), "Curs valutar", 2, "Dates", "Price")
+  ###plot_data(normalize_data( plot_selected(df, ['CHF','EUR','GBP','USD'], '03.01.2020', '29.06.2020')), "Curs valutar", 2, "Dates", "Price")
+  ###plot_data(plot_selected(df, ['CHF','EUR','GBP','USD'], '03.01.2020', '29.06.2020'), "Curs valutar", 2, "Dates", "Price")
+  plot_bollinger_data(df, 'CHF', 20, "Bollinger Bands", 2, "Dates", "Price")
   
 if __name__=="__main__":
   test_run()
